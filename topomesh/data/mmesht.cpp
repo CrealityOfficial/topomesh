b87971554842a61d9056c7b11e70bf4cab53e940
anoob <anoob@sina.cn>
2023-04-07 15:57:19 +0800

    feature:[mesh]init

---
+++ topomesh/data/mmesht.cpp
@@ -0,0 +1,14 @@
+#include "mmesht.h"
+
+namespace topomesh
+{
+	MMeshT::MMeshT()
+	{
+
+	}
+
+	MMeshT::~MMeshT()
+	{
+
+	}
+}
---
+++ topomesh/data/mmesht.h
@@ -0,0 +1,15 @@
+#ifndef TOPOMESH_MMESHT_1680853426715_H
+#define TOPOMESH_MMESHT_1680853426715_H
+#include "trimesh2/Vec.h"
+
+namespace topomesh
+{
+	class MMeshT
+	{
+	public:
+		MMeshT();
+		~MMeshT();
+	};
+}
+
+#endif // TOPOMESH_MMESHT_1680853426715_H
