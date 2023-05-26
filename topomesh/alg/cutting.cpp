3bef0ed9faa3c5d81f71335a69e99a5d1ed1b9f8
anoob <anoob@sina.cn>
2023-05-26 13:12:17 +0800

    feature:[cutting]cpp

---
+++ topomesh/alg/cutting.cpp
@@ -0,0 +1,10 @@
+#include "cutting.h"
+
+namespace topomesh
+{
+	bool ModleCutting(const std::vector<trimesh::TriMesh*>& inMesh, std::vector<trimesh::TriMesh*>& outMesh, const SimpleCamera& camera,
+		const TriPolygon& paths, ccglobal::Tracer* tracer)
+	{
+		return true;
+	}
+}
