671906683528453d62a9c35f0ecbf6f3629c9c88
anoob <anoob@sina.cn>
2023-06-27 13:57:05 +0800

    feature:[internal]

---
+++ topomesh/internal/honeycomb.cpp
@@ -0,0 +1,36 @@
+#include "honeycomb.h"
+#include "topomesh/internal/util.h"
+
+namespace topomesh
+{
+	trimesh::TriMesh* innerGenerateHoneyCombs(trimesh::TriMesh* trimesh, const HoneyCombParam& honeyparams,
+		ccglobal::Tracer* tracer, HoneyCombDebugger* debugger)
+	{
+		if (!trimesh)
+			return;
+
+		MMeshT mesh(trimesh);
+
+		std::vector<int> upperIndex;
+		std::vector<int> lowerIndex;
+		TriPolygons meshPoly;
+
+		if (debugger)
+			debugger->onGenerateBottomPolygons(meshPoly);
+
+		honeyCombLetter(mesh, upperIndex, lowerIndex, meshPoly);
+		toTrimesh(mesh);
+	}
+
+	void honeyCombLetter(MMeshT& mesh, const std::vector<int>& upper, const std::vector<int>& lower,
+		const TriPolygons& meshPoly, HoneyCombDebugger* debugger)
+	{
+		TriPolygons combPoly;
+		TriPolygons result;
+		merge(meshPoly, combPoly, result);
+
+		if (debugger)
+			debugger->onGenerateInfillPolygons(meshPoly);
+		//
+	}
+}
---
+++ topomesh/internal/util.cpp
@@ -0,0 +1,14 @@
+#include "util.h"
+
+namespace topomesh
+{
+	trimesh::TriMesh* toTrimesh(const MMeshT& mesh)
+	{
+		return nullptr;
+	}
+
+	void merge(const TriPolygons& poly1, const TriPolygons& poly2, TriPolygons& out)
+	{
+
+	}
+}
