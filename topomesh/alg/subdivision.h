037785178869aa1d7a5d99fbbae4657d86621816
yangyi <yangyi@cxsw3d.com>
2023-09-08 16:54:54 +0800

    feat：细分顶面
    Change-Id: Ia859747a0afd44b69d20b8992d9177dcd4adc3fe

--- topomesh/alg/subdivision.h
+++ topomesh/alg/subdivision.h
@@ -5,6 +5,7 @@
 
 namespace topomesh {
 	void SimpleMidSubdivision(MMeshT* mesh, std::vector<int>& faceindexs);
+	void SimpleMidSubdivision(trimesh::TriMesh* mesh, std::vector<int>& faceindexs);
 	void loopSubdivision(MMeshT* mesh, std::vector<int>& faceindexs,std::vector<std::tuple<int, trimesh::point>>& vertex,
 		std::vector<std::tuple<int,trimesh::ivec3>>& face_vertex , bool is_move,int iteration);
 	void sqrt3Subdivision(MMeshT* mesh, std::vector<int>& faceindexs);
