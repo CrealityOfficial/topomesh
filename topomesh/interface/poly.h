f05d68fd7d2e0b67e16b6b2933a4d5625cfa0370
zenggui <anoob@sina.cn>
2023-09-08 23:09:41 +0800

    feature:[interface]add simplify

---
+++ topomesh/interface/poly.h
@@ -0,0 +1,10 @@
+#ifndef TOPOMESH_POLY_1692613164094_H
+#define TOPOMESH_POLY_1692613164094_H
+#include "topomesh/interface/idata.h"
+
+namespace topomesh
+{
+	TOPOMESH_API void simplifyPolygons(TriPolygons& polys);
+}
+
+#endif // TOPOMESH_POLY_1692613164094_H
