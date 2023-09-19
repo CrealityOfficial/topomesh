8bdd51c7c51f55adeeb4837cfa94161ef76f2234
anoob <anoob@sina.cn>
2023-09-19 14:56:59 +0800

    feature:[comb]

--- topomesh/alg/fillhoneycombs.h
+++ topomesh/alg/fillhoneycombs.h
@@ -7,6 +7,29 @@
 
 namespace topomesh {
 	
+    struct HoneyCombParam
+    {
+        double resolution = 1E-4; ///<多边形相交允许误差
+        TriPolygon* polyline = nullptr; ///<自由编辑区域生成内蜂窝（默认底面全部填充）
+        trimesh::vec3 axisDir = trimesh::vec3(0, 0, 0); ///<蜂窝多边形平面朝向（默认z轴正方向）
+        //trimesh::vec3* axisDir = nullptr;
+        trimesh::vec2 arrayDir = trimesh::vec2(1, 0); ///<蜂窝多边形平面布局（默认边朝上结构）
+        double honeyCombRadius = 2.0; ///< 生成的蜂窝六边形边长
+        double nestWidth = 1.0; ///<蜂窝六边形壁厚（向内收缩的距离的2倍）
+        double shellThickness = 0.6; ///<抽壳厚度
+        double keepHexagonRate = 0.1; ///允许保留网格的最小面积比例
+        double keepHexagonArea = 2.0; ///允许保留网格的最小面积绝对值参考量
+        bool isdelect = false; //是否删除标记区域
+        std::vector<int> faces; //所选择的区域面，如果为默认-1则为自定义方向
+        bool holeConnect = true;
+        float cheight = 5.0f; ///<圆孔圆心高度
+        float ratio = 0.5f; ///<圆孔直径相对网格边长比例
+        float delta = 0.5f; //圆孔间隔
+        int nslices = 17;   //拟合多边形边数
+        bool bKeepHexagon = false;
+        //debug
+        int step_return = 9999; // debug quick return
+    };
 
 	class HoneyCombDebugger
 	{
--- topomesh/interface/idata.cpp
+++
@@ -1,6 +0,0 @@
-#include "idata.h"
-
-namespace topomesh
-{
-
-}
