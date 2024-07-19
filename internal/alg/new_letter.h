a1089a331b0c535388c9c48749d6d43899c58282
hemiao <hemiao@creality.com>
2024-07-19 15:38:58 +0800

    fix mac build

--- internal/alg/new_letter.h
+++ internal/alg/new_letter.h
@@ -9,8 +9,8 @@ namespace topomesh {
 
 	//生成字体mesh接口 参数1.为字体轮廓、2.字体高度、3.返回每个字的坐标、4.返回每个字的点区间、5.返回每个字的BBX
 	TOPOMESH_API trimesh::TriMesh* CreateFontMesh(const std::vector<std::vector<std::vector<trimesh::vec2>>>& letter, float height,
-		std::vector<float>& word_location = std::vector<float>(), std::vector<int>& mesh_vertex_sizes = std::vector<int>(),
-		std::vector<trimesh::vec3>& word_mesh_center=std::vector<trimesh::vec3>());
+		std::vector<float> word_location = std::vector<float>(), std::vector<int> mesh_vertex_sizes = std::vector<int>(),
+		std::vector<trimesh::vec3> word_mesh_center=std::vector<trimesh::vec3>());
 
 	//根据交互实现字体mesh的变换 参数1.目标mesh、2.字体mesh、3.鼠标点击faceid、4.鼠标点击的坐标、5.面的法线、
 	//6.7.8 上一个接口的字体mesh的三个信息 9、暂定字体上方向 10.是否环绕
